58866b92d858f9abb0171d63dd95dc4d6ccb279b
urivat <rashad.porter18@gmail.com>
2022-04-21 16:25:10 -0400

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPSLSproject
+{
+    public class Player
+    {   
+        //member variables
+        public string name;
+        public int score;
+        public string gesture;
+
+        // constructor
+        public Player(string name , int score , string gesture)
+        {
+            this.name = name;
+            this.score = score;
+            this.gesture = gesture;
+
+        //member methods
+             public string MethodName()
+            { 
+                System.Console.WriteLine('a method to find {0}', name); 
+            
+            }
+        }
+    }
+}
